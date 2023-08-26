a3d1c178013171f33447fa2a4f4ed767d3660690
FajriGoBimbel <fajri.gobimbel@gmail.com>
2023-08-26 09:13:32 +0700

    Create: Go-013-FE, Go-014-FE

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,9 +1,77 @@
 import AuthLayout from '@/components/Layouts/AuthLayout';
+import { Button, Card, Checkbox, Typography, Input } from '@material-tailwind/react';
+import Link from 'next/link';
 import React from 'react'
 
 const SignUp = () => {
+
   return (
-    <div>SignUp</div>
+    <div className='flex justify-center items-center h-screen'>
+      <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Enter your details to Daftar.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <div className="mb-4 flex flex-col gap-6">
+          <Input size="lg" label="Username" />
+          <div className='flex'>
+          <Button
+            ripple={false}
+            variant="text"
+            color="blue-gray"
+            className="flex h-10 items-center gap-2 rounded-r-none border border-r-0 border-blue-gray-200 bg-blue-gray-500/10 pl-3"
+          >
+            +62
+          </Button>
+          <Input
+            type="tel"
+            placeholder="Mobile Number"
+            className="rounded-l-none !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            containerProps={{
+              className: "min-w-0",
+            }}
+          />
+          </div>
+          <Input size="lg" label="Email" />
+          <Input type="password" size="lg" label="Password" />
+          <Input type="password" size="lg" label="Confirm Password" />
+        </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-gray-900"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <Button className="mt-6" fullWidth>
+          Daftar
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <Link href="/auth/signin" className="font-medium text-gray-900">
+            Sign In
+          </Link>
+        </Typography>
+      </form>
+    </Card>
+    </div>
   )
 }
 
